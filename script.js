65452d07056c5fa1aa7b7bf7d661e3391c4dca5f
webknit <contact@webknit.co.uk>
2014-07-09 13:45:19 +0100

    adding initial files

---
+++ script.js
@@ -0,0 +1,44 @@
+/* - BASE HTML TEMPLATE
+------------------------------------------------- 
+	Description: JS Scripts
+	Author:Shane Prendergast
+	Author URL:http://www.webknit.co.uk
+	Template URL:http://base.webknit.co.uk/
+*/
+
+// JS EXAMPLE
+
+var DOND = DOND || {};
+
+DOND.functionName = function()
+{	
+	var self = $(this);
+	var variable = $('.var');
+
+	function init()
+	{
+		variable.click(functionOne);
+		functionTwo();
+	}
+
+	function functionOne()
+	{
+		// JS CODE
+	}
+
+	function functionTwo()
+	{
+		// JS CODE
+		window.hide();
+	}
+
+	init();
+};
+
+// ON DOC READY
+$(function()
+{	
+	new DOND.functionName();
+	
+});
+
