6e0c289f673d3c20d5aa81473955ce5d4538f83d
webknit <contact@webknit.co.uk>
2014-07-10 15:05:37 +0100

    adding coloured boxes

--- script.js
+++ script.js
@@ -12,29 +12,40 @@ var DOND = DOND || {};
 
 DOND.Gameplay = function() {	
 
-	var initialNumbers = $('.initial-numbers');
-	var gameInstructions = $('.game-instructions');
+	// Box rlated variables
+	var gameBoxes = $('.initial-numbers');
+	var boxAmounts = $('.amounts-left');
 	var chosenNumBox = $('.chosen-num-box');
-	var amountsLeftP = $('.amounts-left');
+
+	// Instructions
+	var gameInstructions = $('.game-instructions');
+
+	
+	// Buttons
 	var takeMoneyBtn = $('.take-money');
 	var continueGameBtn = $('.continue-game');
 	var playAgainBtn = $('.play-again');
 	var openBoxesBtn = $('.open-boxes');
 	var swapOpenBoxesBtn = $('.swap-open-boxes');
 
+	// Amounts
 	var initialChosenNum;
 	var chosenBoxAmount;
 
+	// Arrays
 	var boxes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
 	var money = [0.01, 0.10, 0.50, 1, 5, 10, 50, 100, 250, 500, 750, 1000, 3000, 5000, 10000, 15000, 20000, 35000, 50000, 75000, 100000, 250000];
 	var moneyRandom = shuffleArray(money);
 	var amountsLeft;
 	
+	// Moneys and offer amounts
 	var potMoney = 0;
 	var amountOffer = 0;
 
+	// Number of boxes picked
 	var boxesPicked = 0;
 
+	// Call the initial functions
 	function init() {
 
 		addInitialNumbers();
@@ -45,12 +56,14 @@ DOND.Gameplay = function() {
 
 	}
 
+	// Restarts the game
 	function restartGame() {
 
 		location.reload();
 
 	}
 
+	// Array shuffler
 	function shuffleArray(o) {
 
 		for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
@@ -58,6 +71,7 @@ DOND.Gameplay = function() {
 
 	};
 
+	// Exit the game and take the money
 	function takeTheMoney() {
 
 		gameInstructions.empty().append("<p>Congratulations you've won £" + amountOffer + "</p>");
@@ -80,45 +94,54 @@ DOND.Gameplay = function() {
 
 	}
 
+	// Continue player when offered an amount
 	function continueGame() {
 
 		gameInstructions.empty().append("<p>Good choice or not? Lets find out</p>");
 
-		initialNumbers.show();
+		gameBoxes.show();
 		takeMoneyBtn.hide();
 		continueGameBtn.hide();
 
 	}
 
+	// Ask the user deal or no deal
 	function askQuestion () {
 
-		initialNumbers.hide();
+		gameBoxes.hide();
 		takeMoneyBtn.show();
 		continueGameBtn.show();
 
 	}
 
+	// Ask the final question
+	// Could add additional functionality here to swap the boxes
 	function askFinalQuestion () {
 
-		initialNumbers.hide();
+		gameBoxes.hide();
 		takeMoneyBtn.show();
 		continueGameBtn.hide();
 		openBoxesBtn.show();
 
 	}
 
+	// Go all the way and open your box
 	function openBox () {
 
-		initialNumbers.hide();
+		// Hide all buttons, the game has ended
+		gameBoxes.hide();
 		takeMoneyBtn.hide();
 		continueGameBtn.hide();
 		swapOpenBoxesBtn.hide();
 		openBoxesBtn.hide();
 
+
 		gameInstructions.empty().append("<p>You've gone all the way and won £" + chosenBoxAmount + "</p>");
 
+		// Get the last remaining amount in the prize pool
 		var lastAmount = $('.game-active li').data("prize");
 
+		// Conditional statement to user say if won or not
 		if (chosenBoxAmount > lastAmount) {
 
 			chosenNumBox.empty().append("<p>Your box contained " + chosenBoxAmount + ". You beat the banker</p>");
@@ -133,24 +156,27 @@ DOND.Gameplay = function() {
 
 	}
 
+	// Let's play the game
 	function gameActive() {
 
-		initialNumbers.addClass('game-active');
+		// Add class to the 
+		gameBoxes.addClass('game-active');
 
+		// We need to unbind the click as added this game state after
 		$(document).unbind('click').on('click', '.game-active li', function(){
 
+			// The data from the box they picked
 			var moneyRemove = $(this).data("prize");
 			var initialChosenNum = $(this).data("initialchosennum");
 
-			console.log("Value to be removed " + moneyRemove);
-
+			// Update the box selections
 			gameInstructions.empty().append("<p>Your chose box number " + initialChosenNum + " which contained £" + moneyRemove +"</p>");
 
 			// Remove from array
 			// Get the index of the money
 			var remove = moneyRandom.indexOf(moneyRemove);
 
-			// If it's higher than -1 then spice it
+			// If it's higher than -1 then splice it
 			if (remove > -1) {
 
 			    moneyRandom.splice(remove, 1);
@@ -168,7 +194,7 @@ DOND.Gameplay = function() {
 
 			// Output remaining money values
 			// First clear previous ones
-			amountsLeftP.empty();
+			boxAmounts.empty();
 
 			// Sort the array into numerical order
 			moneyRandomSorted = moneyRandom.sort(function(a, b){return a-b}); 
@@ -176,11 +202,11 @@ DOND.Gameplay = function() {
 			// Loop and output remaining values from array
 			for (i = 0; i < moneyRandomSorted.length; i++) {
 
-			    amountsLeftP.show().append("<li>" + moneyRandomSorted[i] + "</li>");
+			    boxAmounts.show().append("<li>" + moneyRandomSorted[i] + "</li>");
 
 			} 
 
-			// If you've picked 5 boxes
+			// The following add the deal or no deal questions on each stage
 			if (boxesPicked == 5) {
 
 				askQuestion();
@@ -207,7 +233,7 @@ DOND.Gameplay = function() {
 
 				askQuestion();
 
-				amountOffer = potMoney * 5 / 100;
+				amountOffer = potMoney * 7 / 100;
 				amountOffer = Math.round(amountOffer);
 
 				gameInstructions.empty().append("<p>Your offer after 11 boxes is £" + amountOffer + ". Deal or no deal?</p>");
@@ -218,7 +244,7 @@ DOND.Gameplay = function() {
 
 				askQuestion();
 
-				amountOffer = potMoney * 8 / 100;
+				amountOffer = potMoney * 10 / 100;
 				amountOffer = Math.round(amountOffer);
 
 				gameInstructions.empty().append("<p>Your offer after 14 boxes is £" + amountOffer + ". Deal or no deal?</p>");
@@ -261,7 +287,7 @@ DOND.Gameplay = function() {
 
 		for (i = 0; i < boxes.length; i++) {
 
-		    initialNumbers.append( "<li data-initialChosenNum=" + boxes[i] + " data-prize=" + moneyRandom[i] + ">Box number " + boxes[i] + "</li>" );
+		    gameBoxes.append( "<li data-initialChosenNum=" + boxes[i] + " data-prize=" + moneyRandom[i] + ">Box number " + boxes[i] + "</li>" );
 
 		}
 
@@ -279,9 +305,20 @@ DOND.Gameplay = function() {
 			moneyRandomSorted = moneyRandom.sort(function(a, b){return a-b}); 
 
 			// Loop and output remaining values from array
+			// If one of the lower boxes we want them to be blue
 			for (i = 0; i < moneyRandomSorted.length; i++) {
 
-			    amountsLeftP.show().append("<li>" + moneyRandomSorted[i] + "</li>");
+				if (moneyRandomSorted[i] < 751) {
+
+					boxAmounts.show().append("<li class='blue'>" + moneyRandomSorted[i] + "</li>");
+
+				}
+
+				else {
+
+					boxAmounts.show().append("<li>" + moneyRandomSorted[i] + "</li>");
+
+				} 
 
 			} 
 
